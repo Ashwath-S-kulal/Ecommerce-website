26578fc561f07cb0ff68c1de6e0627576b887f0c
Ashwath-S-kulal <ashwathkulal2004@gmail.com>
2026-02-15 15:42:36 +0530

    zsc

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,6 +10,11 @@ import persistStore from 'redux-persist/lib/persistStore'
 
 let persistor= persistStore(store)
 
+if ('serviceWorker' in navigator) {
+window.addEventListener('load', () => {
+  navigator.serviceWorker.register('/sw.js');
+});
+} 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
