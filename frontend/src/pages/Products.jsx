e1b499b5736f624e9f845ec66da55cdcef8d2fb2
Ashwath-S-kulal <ashwathkulal2004@gmail.com>
2026-02-15 22:16:36 +0530

    responsive 6

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -202,7 +202,7 @@ export default function Products() {
                     </div>
                 </div>
 
-                <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-3 md:gap-8 '>
+                <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-3 md:gap-4 '>
                     {loading ? (
                         <WishlistSkeleton />
                     ) : currentProducts.length > 0 ? (
