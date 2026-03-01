04ce4f7d51331b26c1c41b23da7746c4522b1128
Ashwath-S-kulal <ashwathkulal2004@gmail.com>
2026-03-01 20:34:12 +0530

    final one

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -290,23 +290,29 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   try {
     const userId = req.id;
-    await Session.deleteMany({ userId: userId });
+
+    if (!userId) {
+      return res.status(400).json({
+        success: false,
+        message: "User not authenticated",
+      });
+    }
+
+    await Session.deleteMany({ userId });
     await User.findByIdAndUpdate(userId, { isLoggedIn: false });
+
     return res.status(200).json({
       success: true,
       message: "User Logged out successfully",
     });
+
   } catch (error) {
-    return (
-      res,
-      status(500).json({
-        success: false,
-        message: error.message,
-      })
-    );
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
-
 
 export const forgotPassword = async (req, res) => {
   try {
