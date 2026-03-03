e02e0d1d91d1a038445d287b76ff3b6cb96678d3
Ashwath-S-kulal <ashwathkulal2004@gmail.com>
2026-03-03 10:06:26 +0530

    again

--- frontend/src/pages/Admin/AddProduct.jsx
+++ frontend/src/pages/Admin/AddProduct.jsx
@@ -44,7 +44,7 @@ const AddProduct = () => {
     formData.append("upload_preset", "your_upload_preset")
 
     const res = await fetch(
-      "https://api.cloudinary.com/v1_1/YOUR_CLOUD_NAME/image/upload",
+      "https://api.cloudinary.com/v1_1/dzapo7ctq/image/upload",
       {
         method: "POST",
         body: formData
