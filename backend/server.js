7425e92719f83429b48561ce279055219d3c8ceb
Ashwath-S-kulal <ashwathkulal2004@gmail.com>
2026-03-03 13:20:26 +0530

    search by name product

--- backend/server.js
+++ backend/server.js
@@ -1,66 +1,27 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import connectDB from "./config/db.js";
-// import userRoute from "./routes/userRoute.js";
-// import productRoute from "./routes/productRoute.js";
-// import cartRoute from "./routes/cartRoute.js";
-// import wishlistRoute from "./routes/wishlistRoute.js";
-// import orderRoute from "./routes/orderRoute.js";
-// import notificationRoutes from "./routes/notificationRoute.js";
-// import addressRoutes from "./routes/addressRoute.js";
-// import cors from "cors";
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(express.json());
-
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URI,
-//     credentials: true,
-//   })
-// );
-
-// app.get("/", (req, res) => {
-//   res.send("Backend running");
-// });
-
-// app.use("/api/user", userRoute);
-// app.use("/api/product", productRoute);
-// app.use("/api/cart", cartRoute);
-// app.use("/api/wishlist", wishlistRoute);
-// app.use("/api/order", orderRoute);
-// app.use("/api/notification", notificationRoutes);
-// app.use("/api/address", addressRoutes);
-
-
-// export default app;
-
-
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRoute from "./routes/userRoute.js";
-import productRoute from "./routes/productRoute.js"
+import productRoute from "./routes/productRoute.js";
 import cartRoute from "./routes/cartRoute.js";
 import wishlistRoute from "./routes/wishlistRoute.js";
 import orderRoute from "./routes/orderRoute.js";
-import notificationRoutes from "./routes/notificationRoute.js"
-import addressRoutes from "./routes/addressRoute.js"
+import notificationRoutes from "./routes/notificationRoute.js";
+import addressRoutes from "./routes/addressRoute.js";
 import cors from "cors";
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 
-app.use(cors({
-  origin: process.env.FRONTEND_URI,
-  credentials: true,
-}
-));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URI,
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Backend running");
@@ -71,15 +32,49 @@ app.use("/api/product", productRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/wishlist", wishlistRoute);
 app.use("/api/order", orderRoute);
-app.use("/api/notification", notificationRoutes)
-app.use("/api/address",addressRoutes)
+app.use("/api/notification", notificationRoutes);
+app.use("/api/address", addressRoutes);
+
+export default app;
 
 
+// import express from "express";
+// import dotenv from "dotenv";
+// import connectDB from "./config/db.js";
+// import userRoute from "./routes/userRoute.js";
+// import productRoute from "./routes/productRoute.js"
+// import cartRoute from "./routes/cartRoute.js";
+// import wishlistRoute from "./routes/wishlistRoute.js";
+// import orderRoute from "./routes/orderRoute.js";
+// import notificationRoutes from "./routes/notificationRoute.js"
+// import addressRoutes from "./routes/addressRoute.js"
+// import cors from "cors";
+// dotenv.config();
 
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+// app.use(express.json());
 
+// app.use(cors({
+//   origin: process.env.FRONTEND_URI,
+//   credentials: true,
+// }
+// ));
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.get("/", (req, res) => {
+//   res.send("Backend running");
+// });
+
+// app.use("/api/user", userRoute);
+// app.use("/api/product", productRoute);
+// app.use("/api/cart", cartRoute);
+// app.use("/api/wishlist", wishlistRoute);
+// app.use("/api/order", orderRoute);
+// app.use("/api/notification", notificationRoutes)
+// app.use("/api/address",addressRoutes)
+
+// app.listen(PORT, () => {
+//   connectDB();
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
